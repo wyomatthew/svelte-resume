7ba0a633a97aab391dd6ef511c497d10251fcc19
Matthew Pickering <wyompick@gmail.com>
2025-03-14 18:20:25 -0500

    feat: handle parse errors
    Signed-off-by: Matthew Pickering <wyompick@gmail.com>

--- src/routes/resume/[path]/+page.server.ts
+++ src/routes/resume/[path]/+page.server.ts
@@ -3,25 +3,34 @@ import * as yaml from 'yaml';
 import type { PageServerLoad } from './$types';
 import * as fs from 'fs';
 import { RESUME_HOME } from '$env/static/private';
-import { resumeSchema, type Resume } from '$lib/resume'
+import { resumeSchema, type Resume } from '$lib/resume';
+import { ZodError } from 'zod';
 
 export const load: PageServerLoad = ({ params }) => {
 	const filePath = RESUME_HOME + '/' + params.path;
 	if (!fs.existsSync(filePath)) {
-		error(404)
+		error(404);
 	}
 
-	const contents = fs.readFileSync(filePath).toString()
+	const contents = fs.readFileSync(filePath).toString();
 	let parsed: Resume;
-	if (filePath.endsWith(".json")) {
-		parsed = resumeSchema.parse(JSON.parse(contents))
-	} else if (filePath.endsWith("yml") || filePath.endsWith("yaml")) {
-		parsed = resumeSchema.parse(yaml.parse(contents.toString()));
-	} else {
-		throw new Error(`unsupported file type ${filePath}`)
+	try {
+		if (filePath.endsWith('.json')) {
+			parsed = resumeSchema.parse(JSON.parse(contents));
+		} else if (filePath.endsWith('yml') || filePath.endsWith('yaml')) {
+			parsed = resumeSchema.parse(yaml.parse(contents.toString()));
+		} else {
+			throw new Error(`unsupported file type ${filePath}`);
+		}
+	} catch (err) {
+		if (err instanceof ZodError) {
+			error(400, `${err.name}: ${err.message}`);
+		}
+
+		error(500)
 	}
 
 	return {
 		resume: parsed
-	}
+	};
 };
