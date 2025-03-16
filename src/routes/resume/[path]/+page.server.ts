0fd29faaca531703e5a3cfbaeb1c9fe24c2c5e94
Matthew Pickering <wyompick@gmail.com>
2025-03-16 14:50:08 -0500

    feat: prerender resume entries
    Signed-off-by: Matthew Pickering <wyompick@gmail.com>

--- src/routes/resume/[path]/+page.server.ts
+++ src/routes/resume/[path]/+page.server.ts
@@ -1,8 +1,8 @@
 import { error } from '@sveltejs/kit';
-import type { PageServerLoad } from './$types';
+import type { PageServerLoad, EntryGenerator } from './$types';
 import { type Resume } from '$lib/resume';
 import { ZodError } from 'zod';
-import { loadResume } from '$lib/server/resume';
+import { loadResume, discoverResumes, resumeHome } from '$lib/server/resume';
 
 export const load: PageServerLoad = ({ params }) => {
 	let parsed: Resume;
@@ -19,4 +19,11 @@ export const load: PageServerLoad = ({ params }) => {
 	return {
 		resume: parsed
 	};
+};
+
+export const entries: EntryGenerator = async () => {
+	const discovered = await discoverResumes(resumeHome);
+	return discovered.map((path) => ({
+		path: path
+	}));
 };
